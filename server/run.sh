546410a4d1a9e27f60c1d00f34d7aea6f82785fe
Johan van Soest <info@johanvansoest.nl>
2020-07-27 20:13:01 +0200

    Updated for file move

--- server/run.sh
+++ server/run.sh
@@ -1,5 +1,4 @@
-mkdir server
-touch server/default.sqlite
+touch containerFiles/default.sqlite
 
 docker run -d \
     --name vantage6_server \
@@ -7,7 +6,7 @@ docker run -d \
     -p 5000:5000 \
     -e "VANTAGE6_DB_URI=sqlite:////default.sqlite" \
     -e "PROXY_SERVER_PORT=80" \
-    -v "$(pwd)/server/config.yaml:/config.yaml" \
-    -v "$(pwd)/server/default.sqlite:/default.sqlite" \
+    -v "$(pwd)/containerFiles/config.yaml:/config.yaml" \
+    -v "$(pwd)/containerFiles/default.sqlite:/default.sqlite" \
     -v "/var/run/docker.sock:/var/run/docker.sock" \
     harbor.vantage6.ai/infrastructure/server vserver-local start -c /config.yaml
