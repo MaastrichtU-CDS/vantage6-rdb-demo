973156cb75fe7649a846f3d0704f80fccc9c35db
Johan van Soest <info@johanvansoest.nl>
2020-08-04 14:04:09 +0200

    Fixed typo

--- maastro/run.sh
+++ maastro/run.sh
@@ -9,7 +9,7 @@ docker run -d \
     -v $(pwd)/log:/mnt/log \
     -v ${datavolume_name}:/mnt/data \
     -v $(pwd)/config:/mnt/config \
-    -v $(pwd)/data/Clinicial1.csv:/mnt/database.csv \
+    -v $(pwd)/data/Clinical1.csv:/mnt/database.csv \
     -v /var/run/docker.sock:/var/run/docker.sock \
     --link vantage6_server \
     harbor.distributedlearning.ai/infrastructure/node vnode-local start -c /mnt/config/config.yaml
